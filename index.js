58390d7242c9d510c89ff0f2feb6d4a438f9a268
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-01-07 18:26:10 -0500

    got questions working but not writing to readme

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution',
+    },
+    {
+        type: 'input',
+        name: 'deployedlink',
+        message: 'Deployed Link',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose license',
+        choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", 'BSD 2-Clause "Simplified" License',
+        'BSD 3-Clause "New" or "Revised" License', "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "GNU General Public License v2.0", 
+        "Mozilla Public License 2.0", "The Unlicense"]   
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'GitHub username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email address',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(__dirname, '/dist/', fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt (questions).then(answers => {
+        writeToFile('README.md', generateMarkdown(answers))
+        console.log(answers)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache License 2.0') {
+    return "[![Apache](https://img.shields.io/badge/license-apache-yellow)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (license === "GNU General Public License v3.0") {
+    return "![GNU](https://img.shields.io/badge/license-gnu-blue)"
+  } else if (license === "MIT License") {
+    return "![MIT](https://img.shields.io/badge/license-mit-blue)"
+  } else if (license === 'BSD 2-Clause "Simplified" License') {
+    return "![BSD](https://img.shields.io/badge/license-bsd-orange)"
+  } else if (license === 'BSD 3-Clause "New" or "Revised" License') {
+    return "![BSD](https://img.shields.io/badge/license-bsd-orange)"
+  } else if (license === "Boost Software License 1.0") {
+    return "![BOOST](https://img.shields.io/badge/license-boost-green)"
+  } else if (license === "Creative Commons Zero v1.0 Universal") {
+    return "![CCU](https://img.shields.io/badge/license-ccu-green)"
+  } else if (license === "GNU General Public License v2.0") {
+    return "![GNU](https://img.shields.io/badge/license-gnu-blue)"
+  } else if (license === "Mozilla Public License 2.0") {
+    return "![MPL](https://img.shields.io/badge/license-mpl-purple)"
+  } else if (license === "The Unlicense") {
+    return "![UNL](https://img.shields.io/badge/license-unl-purple)"
+  }
+  else {
+    return " "
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return " "
+  } else {
+    return "- [License](#license)"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
